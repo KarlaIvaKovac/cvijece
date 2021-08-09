520a34004b06c06ff6e35d201a7eb6b2e292bb06
Karla <kovackarla8@gmail.com>
2021-08-09 14:00:55 +0200

    Tablica
    Napravljena tablica s popisom ljudi

---
+++ src/komponente/BasicTable.js
@@ -0,0 +1,56 @@
+import React, {useMemo} from 'react'
+import { useTable } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { STUPCI } from './columns'
+import './table.css'
+
+export const BasicTable = () =>{
+
+    // preporuceno koristiti use memo hook ?????
+
+    const columns = useMemo(()=> STUPCI, [])
+    const data = useMemo(() => MOCK_DATA, [])
+
+    const tableInstance = useTable({
+        columns, //columns = const columns = STUPCI
+        data
+    })
+
+    const {
+        getTableProps, 
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance
+
+
+    return(
+        <table {...getTableProps()}>
+            <thead> 
+                {headerGroups.map((headerGroups) => (                                   
+                <tr {...headerGroups.getHeaderGroupProps()}>
+                    {
+                        headerGroups.headers.map((column) => (
+                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}                  
+                </tr> 
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row=> {
+                        prepareRow(row)
+                        return(
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map((cell) => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })}                                
+                            </tr>
+                        )
+                    })}              
+            </tbody>
+        </table>               
+    )
+}
---
+++ src/komponente/columns.js
@@ -0,0 +1,22 @@
+export const STUPCI = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Ime',
+        accessor: 'ime'
+    },
+    {
+        Header: 'Prezime',
+        accessor: 'prezime'
+    },
+    {
+        Header: 'Drzava',
+        accessor: 'drzava'
+    },
+    {
+        Header: 'Boja buketa',
+        accessor: 'bojaBuketa'
+    }
+]
